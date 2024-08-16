dbc42353c716365a9e78d7e2b084346affc96e88
zhero <zheronkina86@gmail.com>
2024-08-16 22:45:30 +0300

    часть проекта

---
+++ calculator/src/test/java/pro/sky/calculator/service/CalculatorServiceParametrizedTest.java
@@ -0,0 +1,35 @@
+package pro.sky.calculator.service;
+
+import net.bytebuddy.asm.MemberSubstitution;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class CalculatorServiceParametrizedTest {
+
+    CalculatorService calculatorService = new CalculatorServiceImpl();
+
+    static Stream <Arguments> data(){
+        return Stream.of(
+                Arguments.of(10, 2),
+                Arguments.of(10, -2),
+                Arguments.of(-10, -2),
+                Arguments.of(-200, 5),
+                Arguments.of(222, 2),
+                Arguments.of(-200, -5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void plus(int a, int b) {
+        int actual = calculatorService.plus(a, b);
+        int expected = a + b;
+        Assertions.assertEquals(actual, expected);
+    }
+
+
+}
